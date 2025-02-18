b1d7aa0bae0681692c95a12c44864caada47a2c4
bojin11 <15011278723@163.com>
2025-02-18 12:03:26 +0000

    project plan added

--- src/components/api.js
+++ src/components/api.js
@@ -15,3 +15,15 @@ export const getUsers = () => {
     return res.data.users;
   });
 };
+
+export const getArticleById = (article_id) => {
+  return ncNewsApi.get(`/articles/${article_id}`).then((res) => {
+    return res.data.article;
+  });
+};
+
+export const getArticleCommentsById = (article_id) => {
+  return ncNewsApi.get(`/articles/${article_id}/comments`).then((res) => {
+    return res.data.comments;
+  });
+};
